ff17256169590bffc640acb5672376d120f08211
Rinatzzak <zakirov.zrz@gmail.com>
2023-01-11 19:22:02 +0300

    refactor main class for spring boot

---
+++ cbrRate/src/main/java/ru/rinattzak/CbrRate.java
@@ -0,0 +1,11 @@
+package ru.rinattzak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbrRate {
+    public static void main(String[] args) {
+        SpringApplication.run(CbrRate.class);
+    }
+}
