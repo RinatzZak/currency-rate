041231ba9802b9a98c95cdfd727707b9e8d44ff5
Rinatzzak <zakirov.zrz@gmail.com>
2023-01-12 18:03:13 +0300

    add requester interface and impl

---
+++ cbrRate/src/main/java/ru/rinattzak/requester/CbrRequester.java
@@ -0,0 +1,5 @@
+package ru.rinattzak.requester;
+
+public interface CbrRequester {
+    String getRatesAsXml(String url);
+}
---
+++ cbrRate/src/main/java/ru/rinattzak/requester/CbrRequesterImpl.java
@@ -0,0 +1,34 @@
+package ru.rinattzak.requester;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.rinattzak.exception.RequesterException;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+@Slf4j
+public class CbrRequesterImpl implements CbrRequester {
+    @Override
+    public String getRatesAsXml(String url) {
+        try {
+            log.info("request for url{}", url);
+            var client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (Exception e) {
+            if (e instanceof InterruptedException) {
+                Thread.currentThread().interrupt();
+            }
+            log.error("request error, url{}", url, e);
+            throw new RequesterException(e);
+        }
+    }
+}
