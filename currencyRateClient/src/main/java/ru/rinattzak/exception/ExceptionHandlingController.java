ca1e88a649a247b94bf825d946c3059f41fa3023
Rinatzzak <zakirov.zrz@gmail.com>
2023-01-14 17:03:23 +0300

    add custom exception handler

---
+++ currencyRateClient/src/main/java/ru/rinattzak/exception/ExceptionHandlingController.java
@@ -0,0 +1,16 @@
+package ru.rinattzak.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlingController {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public String serverException(Exception e) {
+        return e.getMessage();
+    }
+}
